7deec01ea404ca43e744feacaa2bc3f6070d4ff2
mkindia <manojkumarindia@yahoo.com>
2026-01-26 15:21:25 +0530

    27-01-26

--- Django/accounting/ledger/admin.py
+++
@@ -1,5 +0,0 @@
-from django.contrib import admin
-from .models import Company,Ledger
-admin.site.register(Company)
-admin.site.register(Ledger)
-# admin.site.register(AccountingGroup)
--- Django/accounting/transaction/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin # type: ignore
-from .models import Transaction, TransactionEntry
-admin.site.register(Transaction)
-
-class Entry(admin.ModelAdmin):  #for Deletion Protect in admin panel
-    def has_delete_permission(self, request, obj=None):
-        return False
-               
-admin.site.register(TransactionEntry, Entry)
